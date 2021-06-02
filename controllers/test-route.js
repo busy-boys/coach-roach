f7b540d85c0a7d0b244b0ef002a1dbb1105b8263
Dave Holst <daveholst@gmail.com>
2021-06-02 18:04:11 +0800

    auto deploy - try 2

--- controllers/test-route.js
+++ controllers/test-route.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
   try {
     await sequelize.authenticate();
-    res.json('Try1 server is up! \n database is up!');
+    res.json('Try2 - server is up! \n database is up!');
   } catch (error) {
     res.status(500).json('error connectigng to db', error);
   }
