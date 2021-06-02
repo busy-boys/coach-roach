a089524d82ecac398848db3aa16283e5c1f273b7
Dave Holst <daveholst@gmail.com>
2021-06-02 16:43:42 +0800

    db connection test

--- controllers/test-route.js
+++ controllers/test-route.js
@@ -1,7 +1,14 @@
+const sequelize = require('../config/connection');
+
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.json('server is up!');
+router.get('/', async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.json('server is up! \n database is up!');
+  } catch (error) {
+    res.status(500).json('error connectigng to db', error);
+  }
 });
 
 module.exports = router;
