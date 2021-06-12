7dbfd06ca1c18cb8045c333847959e497d7acdcf
Dave Holst <daveholst@gmail.com>
2021-06-12 10:38:30 +0800

    A.10.10: basic doughnut graph to show trainging type splits

--- html-pages/graph-dashboard-test.js
+++ html-pages/graph-dashboard-test.js
@@ -1,12 +1,23 @@
 const chart1 = document.getElementById('myChart').getContext('2d');
+const chart2 = document.getElementById('myChart2').getContext('2d');
+// TODO hit an api to get data object then reference below when building object/Class
+
 const myChart = new Chart(chart1, {
   type: 'bar',
   data: {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    // labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
     datasets: [
       {
         label: 'Hrs of Coaching',
-        data: [12, 19, 3, 5, 2, 3, 6],
+        data: [
+          { month: 'jan', hours: 12 },
+          { month: 'feb', hours: 19 },
+          { month: 'mar', hours: 3 },
+          { month: 'apr', hours: 5 },
+          { month: 'may', hours: 2 },
+          { month: 'jun', hours: 3 },
+          { month: 'jul', hours: 6 },
+        ],
         backgroundColor: ['rgba(255, 206, 86, 0.2)'],
         borderColor: ['rgba(255, 206, 86, 1)'],
         borderWidth: 3,
@@ -15,6 +26,10 @@ const myChart = new Chart(chart1, {
     ],
   },
   options: {
+    parsing: {
+      xAxisKey: 'month',
+      yAxisKey: 'hours',
+    },
     scales: {
       y: {
         beginAtZero: true,
@@ -42,10 +57,69 @@ const myChart = new Chart(chart1, {
       annotation: {
         annotations: {
           line1: {
+            drawTime: 'afterDatasetsDraw',
             type: 'line',
             yMin: 6,
             yMax: 6,
             borderColor: 'rgb(95,214,0)',
+          },
+        },
+      },
+    },
+  },
+});
+
+const myChart2 = new Chart(chart2, {
+  type: 'doughnut',
+  data: {
+    labels: [
+      'Training Type 1',
+      'Training Type 2',
+      'Training Type 3',
+      'Training Type 4',
+      'Training Type 5',
+      'Training Type 6',
+    ],
+    datasets: [
+      {
+        label: 'Coaching Type',
+        data: [0, 3, 8, 9, 15, 6],
+        // ! DH - rank colours! Need to changed!!!
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 205, 86, 0.6)',
+          'rgba(152, 85, 86, 0.6)',
+          'rgba(54, 65, 186, 0.6)',
+          'rgba(202, 128, 66, 0.6)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 205, 86, 1)',
+          'rgba(152, 85, 86, 1)',
+          'rgba(54, 65, 186, 1)',
+          'rgba(202, 128, 66, 1)',
+        ],
+        hoverOffset: 30,
+        borderWidth: 3,
+        borderRadius: 2,
+      },
+    ],
+  },
+  options: {
+    layout: {
+      padding: 50,
+    },
+    plugins: {
+      // ! DH - Not sure about the legend?
+      legend: {
+        display: false,
+        position: 'right',
+        labels: {
+          // This more specific font property overrides the global property
+          font: {
+            size: 14,
           },
         },
       },
