fddcf342c2963654d102112ed357c5433ae4fd5f
Dave Holst <daveholst@gmail.com>
2021-06-14 08:20:30 +0800

    A.10.10: added user create route on POST /api/user for #15

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const topLevelRoutes = require('./top-level-routes.js');
+const apiRoutes = require('./api');
 
 router.use('/', topLevelRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
