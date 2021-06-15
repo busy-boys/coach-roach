545e4360fff773ea2e3395ce97a471e757bc4341
rorylkd <rory.donaldson14@gmail.com>
2021-06-15 15:57:47 +0800

    C.10.10-coaching-CRUD-API Wrote skeleton for /coaching route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const coachingRoutes = require('./coaching-routes');
 
 router.use('/user', userRoutes);
+router.use('/coaching', coachingRoutes);
 
 module.exports = router;
