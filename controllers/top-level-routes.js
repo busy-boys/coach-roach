8a408b738a06fd398067a7e16f16d1e678c0c3e9
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-06-16 19:41:14 +0800

    C.30.10: now getting names for roles of coaching session

--- controllers/top-level-routes.js
+++ controllers/top-level-routes.js
@@ -56,70 +56,77 @@ router.get('/mysessions', async (req, res) => {
     // Get PAST that are signed off.
 
     // Get the loggedIn users session data.
-    const dbSessionsData = await User.findByPk(1002, {
+    const testID = 1002;
+    const dbSessionsData = await CoachingSession.findAll({
+      where: {
+        [Op.or]: {
+          senior_coordinator_id: testID,
+          supervisor_id: testID,
+          superintendent_id: testID,
+        },
+      },
       include: [
         {
-          model: CoachingSession,
-          as: 'senior_coordinator_sessions',
+          model: User,
+          as: 'senior_coordinator',
           // include: [{ model: User, as: 'senior_coordinator' }],
         },
         {
-          model: CoachingSession,
-          as: 'superintendent_sessions',
+          model: User,
+          as: 'superintendent',
           // include: [{ model: User, as: 'superintendent' }],
         },
         {
-          model: CoachingSession,
-          as: 'supervisor_sessions',
-          // where: { supervisor_id: id },
-          include: [{ model: User, as: 'supervisor' }],
+          model: User,
+          as: 'supervisor',
         },
       ],
     });
     // clean it up
-    const userSessions = dbSessionsData.get({ plain: true });
+
+    const userSessions = dbSessionsData.map((session) =>
+      session.get({ plain: true })
+    );
+
     console.log(userSessions);
     // add all sessions together into one array
-    const allUserSessions = userSessions.senior_coordinator_sessions.concat(
-      userSessions.superintendent_sessions,
-      userSessions.supervisor_sessions
-    );
+
     // console.log(allUserSessions);
     // sort out sessions
     const scheduledSessions = [];
     const pendingSignOff = [];
     const pastTraining = [];
 
-    await allUserSessions.forEach(async (session) => {
-      const sessionTime = new Date(session.start_time);
-      const nowTime = new Date();
-      // get names of session members
-      // const seniorCordinator = await User.findByPk(
-      //   session.senior_coordinator_id
-      // );
-      // session.senior_cordinator = seniorCordinator.get({ plain: true });
+    // await allUserSessions.forEach(async (session) => {
+    //   const sessionTime = new Date(session.start_time);
+    //   const nowTime = new Date();
+    //   // get names of session members
+    //   // const seniorCordinator = await User.findByPk(
+    //   //   session.senior_coordinator_id
+    //   // );
+    //   // session.senior_cordinator = seniorCordinator.get({ plain: true });
 
-      // const supervisor = await User.findByPk(session.supervisor_id);
-      // session.supervisor = supervisor.get({ plain: true });
+    //   // const supervisor = await User.findByPk(session.supervisor_id);
+    //   // session.supervisor = supervisor.get({ plain: true });
 
-      // const superintendent = await User.findByPk(session.superintendent_id);
-      // session.superintendent = superintendent.get({ plain: true });
+    //   // const superintendent = await User.findByPk(session.superintendent_id);
+    //   // session.superintendent = superintendent.get({ plain: true });
 
-      // triage results.
-      if (sessionTime.getTime() >= nowTime.getTime()) {
-        scheduledSessions.push(session);
-      } else if (
-        sessionTime.getTime() < nowTime.getTime() &&
-        ((!session.senior_coordinator_signedOff &&
-          session.senior_coordinator_id) ||
-          (!session.supervisor_signedOff && session.supervisor_id) ||
-          (!session.superintendent_signedOff && session.superintendent_id))
-      ) {
-        pendingSignOff.push(session);
-      } else {
-        pastTraining.push(session);
-      }
-    });
+    //   // triage results.
+    //   if (sessionTime.getTime() >= nowTime.getTime()) {
+    //     scheduledSessions.push(session);
+    //   } else if (
+    //     sessionTime.getTime() < nowTime.getTime() &&
+    //     ((!session.senior_coordinator_signedOff &&
+    //       session.senior_coordinator_id) ||
+    //       (!session.supervisor_signedOff && session.supervisor_id) ||
+    //       (!session.superintendent_signedOff && session.superintendent_id))
+    //   ) {
+    //     pendingSignOff.push(session);
+    //   } else {
+    //     pastTraining.push(session);
+    //   }
+    // });
     console.log(scheduledSessions);
     // console.log(pendingSignOff);
     // console.log(pastTraining);
