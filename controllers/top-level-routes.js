b93c6c35ee063a78584eb3cfcd912f61b584ad51
rorylkd <rory.donaldson14@gmail.com>
2021-06-17 19:58:43 +0800

    A.10.30-mystats-page Created graphData object

--- controllers/top-level-routes.js
+++ controllers/top-level-routes.js
@@ -148,23 +148,38 @@ router.get('/mystats', async (req, res) => {
     });
     const cleanData = dbSessionData.map((data) => data.get({ plain: true }));
 
-    // (jan is 0, june is 5).
-    // This will give me all the values in the cleanData array where start_time has a month value of june.
-    // Eventually there will be a switch statement or something here to get the values for each month.
-    const getAllValuesPerMonth = cleanData.filter(
-      (data) => data.start_time.getMonth() === 0
-    );
-    console.log('getAllValuesPerMonth', getAllValuesPerMonth);
+    const totalHoursForAllMonths = [];
+    for (let i = 0; i < 12; i++) {
+      const getAllHoursPerMonth = cleanData.filter(
+        (data) => data.start_time.getMonth() === i
+      );
 
-    // THis will grab all the values for the duration parameter in every June session.
-    const getAllMonthlyHours = getAllValuesPerMonth.map((a) => a.duration);
-    console.log(getAllMonthlyHours);
+      // THis will grab all the values for the duration parameter in every June session.
+      const getAllMonthlyHours = getAllHoursPerMonth.map((a) => a.duration);
 
-    // This adds them up to get total hours per month. This value needs to be passed to the graphing function.
-    const sumOfHoursPerMonth = getAllMonthlyHours.reduce((a, b) => a + b, 0);
-    console.log(sumOfHoursPerMonth);
+      // This adds them up to get total hours per month. This value needs to be passed to the graphing function.
+      const sumOfHoursPerMonth = getAllMonthlyHours.reduce((a, b) => a + b, 0);
 
-    res.render('my-stats', { cleanData });
+      // Adding
+      totalHoursForAllMonths.push(sumOfHoursPerMonth);
+    }
+
+    const graphData = {
+      January: totalHoursForAllMonths[0],
+      February: totalHoursForAllMonths[1],
+      March: totalHoursForAllMonths[2],
+      April: totalHoursForAllMonths[3],
+      May: totalHoursForAllMonths[4],
+      June: totalHoursForAllMonths[5],
+      July: totalHoursForAllMonths[6],
+      August: totalHoursForAllMonths[7],
+      September: totalHoursForAllMonths[8],
+      October: totalHoursForAllMonths[9],
+      November: totalHoursForAllMonths[10],
+      December: totalHoursForAllMonths[11],
+    };
+    console.log('graphData:', graphData);
+    res.render('my-stats', { graphData });
   } catch (err) {
     res.status(500).json(err);
   }
