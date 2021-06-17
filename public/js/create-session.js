6f3f1b318797013169381d74895fb962696677d5
Ritz GL <maurigarzonlara@hotmail.com>
2021-06-17 16:13:01 +0800

    POST request throwing errors on form submission

--- public/js/create-session.js
+++ public/js/create-session.js
@@ -3,23 +3,12 @@ const createSessionForm = document.querySelector('#booking-form');
 
 createSessionForm.addEventListener('submit', async (event) => {
   event.preventDefault();
-  //   console.log(event.target);
-  //   const data = new FormData(event.target);
-  //   const bookingData = Object.fromEntries(data.entries());
-  const dummyData = {
-    date: '2021-06-30T12:20',
-    duration: '120',
-    location: 'Murdoch',
-    senior_coordinator_id: null,
-    superintendent_id: null,
-    supervisor_id: null,
-    topic: 'Safety Hazard',
-  };
-  //   const d = JSON.stringify(dummyData);
-  console.log("I don't fucking know what's wrong", dummyData);
+  console.log(event.target);
+  const data = new FormData(event.target);
+  const bookingData = Object.fromEntries(data.entries());
   try {
     console.log('before request');
-    const res = await axios.post('/api/coaching', dummyData);
+    const res = await axios.post('/api/coaching', bookingData);
     console.log('after request');
     console.log(res);
   } catch (err) {
