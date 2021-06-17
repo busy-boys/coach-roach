1f58972a72224983c040a74ba55dd9e0f41b3a29
rorylkd <rory.donaldson14@gmail.com>
2021-06-17 20:36:51 +0800

    A.10.20-mystats-page Adding mygraphdata api route; moving mystats total hours logic over

--- controllers/top-level-routes.js
+++ controllers/top-level-routes.js
@@ -125,63 +125,10 @@ router.get('/mysessions', authCheck, async (req, res) => {
   }
 });
 router.get('/mystats', async (req, res) => {
-  const testID = [1005];
   try {
-    const dbSessionData = await CoachingSession.findAll({
-      attributes: [
-        'start_time',
-        'duration',
-        'senior_coordinator_id',
-        'supervisor_id',
-        'superintendent_id',
-      ],
-      where: {
-        // Getting all sessions where complete = true AND
-        // where one of the participants have an ID that matches testID
-        [Op.or]: {
-          senior_coordinator_id: testID,
-          supervisor_id: testID,
-          superintendent_id: testID,
-        },
-        [Op.and]: { complete: true },
-      },
-    });
-    const cleanData = dbSessionData.map((data) => data.get({ plain: true }));
-
-    const totalHoursForAllMonths = [];
-    for (let i = 0; i < 12; i++) {
-      const getAllHoursPerMonth = cleanData.filter(
-        (data) => data.start_time.getMonth() === i
-      );
-
-      // THis will grab all the values for the duration parameter in every June session.
-      const getAllMonthlyHours = getAllHoursPerMonth.map((a) => a.duration);
-
-      // This adds them up to get total hours per month. This value needs to be passed to the graphing function.
-      const sumOfHoursPerMonth = getAllMonthlyHours.reduce((a, b) => a + b, 0);
-
-      // Adding
-      totalHoursForAllMonths.push(sumOfHoursPerMonth);
-    }
-
-    const graphData = {
-      January: totalHoursForAllMonths[0],
-      February: totalHoursForAllMonths[1],
-      March: totalHoursForAllMonths[2],
-      April: totalHoursForAllMonths[3],
-      May: totalHoursForAllMonths[4],
-      June: totalHoursForAllMonths[5],
-      July: totalHoursForAllMonths[6],
-      August: totalHoursForAllMonths[7],
-      September: totalHoursForAllMonths[8],
-      October: totalHoursForAllMonths[9],
-      November: totalHoursForAllMonths[10],
-      December: totalHoursForAllMonths[11],
-    };
-    console.log('graphData:', graphData);
-    res.render('my-stats', { graphData });
+    res.render('my-stats');
   } catch (err) {
-    res.status(500).json(err);
+    console.error(err);
   }
 });
 // TODO add route for manager
